3dd109d6fc176115c3c0d234df1fc81508649919
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-10-18 19:12:15 -0300

    challenge 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,5 @@
+const totalProdutos = db.produtos.countDocuments({});
+
+db.resumoProdutos.insertOne({ franquia: "McDonalds", totalProdutos });
+
+db.resumoProdutos.find({}, { _id: 0, franquia: 1, totalProdutos: 1 });
