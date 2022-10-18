989952dab1a8366c98ffd531c6fa87e98d02b981
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-10-18 19:00:40 -0300

    challenge 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 }).pretty();
