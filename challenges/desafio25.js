bdc4b64b51defe8481d7a027f9e1c65d8b98350b
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-10-19 21:38:16 -0300

    challenge 25

---
+++ challenges/desafio25.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany(
+    { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
+    { $push: { tags: "muito sódio" } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
